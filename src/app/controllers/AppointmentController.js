0699482773582133ffd00f37ff4883841043e27e
Alexandre Busarello <busamar@LAPTOP-NKAJQ9SE.localdomain>
2019-07-02 20:14:26 +0000

    feature: Create route store of appointment

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,62 @@
+import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    const checkIsProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ error: 'You can only create a appointments with providers' });
+    }
+
+    /**
+     * Check for past date
+     */
+    const startHour = startOfHour(parseISO(date));
+    if (isBefore(startHour, new Date())) {
+      return res.status(400).json({ error: 'Past date is not allowed' });
+    }
+
+    /**
+     * Check date availability
+     */
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: startHour,
+      },
+    });
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
